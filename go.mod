2a15bd7fef6bc57415bf417bc7ebfc98d3f129c4
fwiedmann <privat@felixwiedmann.de>
2023-12-20 18:30:25 +0100

    feat: add gitlab client

--- go.mod
+++ go.mod
@@ -17,6 +17,10 @@ require (
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.4.0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
@@ -24,10 +28,15 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/skeema/knownhosts v1.1.0 // indirect
+	github.com/xanzy/go-gitlab v0.95.2 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
-	golang.org/x/net v0.2.0 // indirect
-	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.29.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/gitlab/client.go
+++ pkg/gitlab/client.go
@@ -1,4 +1,88 @@
 package gitlab
 
+import (
+	"github.com/fwiedmann/dispatch"
+	"github.com/xanzy/go-gitlab"
+)
+
+func NewClient(url, token, projectId string, mergeRequestId int) (Client, error) {
+	c, err := gitlab.NewClient(token, gitlab.WithBaseURL(url))
+	if err != nil {
+		return Client{}, err
+	}
+	return Client{
+		gitLabApiClient: c,
+	}, nil
+}
+
 type Client struct {
+	gitLabApiClient *gitlab.Client
+	projectId       string
+	mergeRequestId  int
+}
+
+func (c *Client) Dispatch(notes []dispatch.Note) error {
+	userIds, err := c.getUserIds(notes)
+	if err != nil {
+		return err
+	}
+
+	return c.updateMergeRequest(userIds, notes)
+}
+
+func (c *Client) getUserIds(notes []dispatch.Note) ([]int, error) {
+	userIdsMap := map[int]any{}
+
+	list, _, err := c.gitLabApiClient.ProjectMembers.ListAllProjectMembers(c.projectId, &gitlab.ListProjectMembersOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, note := range notes {
+		for _, member := range note.Members {
+			for _, projectMember := range list {
+				if member == projectMember.Email {
+					userIdsMap[projectMember.ID] = struct{}{}
+				}
+			}
+		}
+	}
+
+	userIds := make([]int, 0)
+	for k := range userIdsMap {
+		userIds = append(userIds, k)
+	}
+	return userIds, nil
+}
+
+func (c *Client) updateMergeRequest(memberIds []int, notes []dispatch.Note) error {
+	mr, _, err := c.gitLabApiClient.MergeRequests.GetMergeRequest(c.projectId, c.mergeRequestId, nil)
+	if err != nil {
+		return err
+	}
+
+	updateOptions := &gitlab.UpdateMergeRequestOptions{}
+	updateOptions.AssigneeIDs = c.addMissingAssignees(memberIds, mr.Assignees)
+
+	_, _, err = c.gitLabApiClient.MergeRequests.UpdateMergeRequest(c.projectId, c.mergeRequestId, updateOptions)
+	return err
+}
+
+func (c *Client) addMissingAssignees(memberIds []int, existingAssignees []*gitlab.BasicUser) *[]int {
+	updatedAssignees := make([]int, 0)
+	for _, assignee := range existingAssignees {
+		updatedAssignees = append(updatedAssignees, assignee.ID)
+	}
+	for _, memberId := range memberIds {
+		found := false
+		for _, assigneeId := range updatedAssignees {
+			if memberId == assigneeId {
+				found = true
+			}
+		}
+		if !found {
+			updatedAssignees = append(updatedAssignees, memberId)
+		}
+	}
+	return &updatedAssignees
 }
